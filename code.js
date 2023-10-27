b37addac3b93f98da071bd9837a799408e80b227
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-10-26 23:26:28 -0600

    1st attempt

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    var adjList = [];
+
+    for (var i = 0; i < adjMatrix.length; i++) {
+        var innerList = [];
+
+        for (var j = 0; j < adjMatrix[i].length; j++) {
+            if (adjMatrix[i][j] > 0) {
+                innerList.push(j);
+            }
+        }
+        adjList[i] = innerList;
+    }
+
+    return adjList;
 }
+
+
+var adjMatrix = [
+    [0, 1, 1],
+    [0, 0, 1],
+    [1, 0, 0]
+];
+
+var adjList = convertToAdjList(adjMatrix);
+
+console.log(adjList);
